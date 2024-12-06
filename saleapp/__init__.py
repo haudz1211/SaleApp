1029c783c0a27d6030a507a66dd47847d302d875
haudz <trinhdoanhauu@gmail.com>
2024-12-06 15:59:55 +0700

    update 6/12

--- saleapp/__init__.py
+++ saleapp/__init__.py
@@ -1,11 +1,20 @@
+import cloudinary
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import quote
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+app.secret_key = "%^$DSD^%^%^%^%^DSSD"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/saledb?charset=utf8mb4" % quote('Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 2
 
+
+login = LoginManager(app)
 db = SQLAlchemy(app)
+
+
+cloudinary.config(cloud_name='dgeyq5bpg',
+                  api_key='124191785282462',
+                  api_secret='-FwQxZuQW-YziPfCZ3ktdIkqeug')
--- saleapp/index.py
+++ saleapp/index.py
@@ -1,24 +1,78 @@
-from flask import Flask, render_template, request
+import math
+from flask import render_template, request, redirect
 import dao
+from saleapp import app,admin, login
+from flask_login import login_user, logout_user, current_user
+import cloudinary.uploader
 
-from saleapp import app
 
-
-@app.route("/")
+@app.route('/')
 def index():
     q = request.args.get("q")
     cate_id = request.args.get("category_id")
     page = request.args.get("page")
-    pages = dao.count_product()
-    products = dao.load_products(q =q , cate_id= cate_id, page= page)
-    return render_template("index.html", products=products, pages = (int) (pages/app.config["PAGE_SIZE"]))
+    products = dao.load_products(q=q, cate_id=cate_id, page=page)
+    total = dao.count_products()
+
+    return render_template('index.html', products=products, pages=math.ceil(total/app.config['PAGE_SIZE']))
 
 
-@app.route("/products/<int:id>")
-def product_details(id):
+@app.route('/products/<int:id>')
+def details(id):
     product = dao.load_product_by_id(id)
-    print(product)
-    return render_template("product-details.html", product= product)
+    return render_template('product-details.html', product = product)
+
+
+@app.route('/login', methods=['get', 'post'])
+def login_my_user():
+    if current_user.is_authenticated:
+        return redirect("/")
+
+    err_msg = None
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = dao.auth_user(username,password)
+        if user:
+            login_user(user)
+            return redirect('/')
+        else:
+            err_msg = "Tài khoản hoặc mật khẩu không đúng!"
+
+    return render_template('login.html', err_msg=err_msg)
+
+
+@app.route('/logout')
+def logout_my_user():
+    logout_user()
+    return redirect('/login')
+
+
+@app.route('/register', methods=['get', 'post'])
+def register():
+    err_msg = None
+    if request.method.__eq__('POST'):
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        if password.__eq__(confirm):
+            username = request.form.get("username")
+            name = request.form.get("name")
+            avatar = request.files.get('avatar')
+            avatar_path = None
+            if avatar:
+                res = cloudinary.uploader.upload(avatar)
+                avatar_path = res['secure_url']
+            dao.add_user(name=name, username=username, password=password, avatar=avatar_path)
+            return redirect('/login')
+        else:
+            err_msg = "Mật khẩu không khớp!"
+
+    return render_template('register.html', err_msg=err_msg)
+
+
+@login.user_loader
+def load_user(user_id):
+    return dao.get_user_by_id(user_id)
 
 
 @app.context_processor
@@ -27,13 +81,6 @@ def common_attributes():
         "categories": dao.load_categories()
     }
 
-
-@app.route("/login")
-def login_my_user():
-    return render_template("login.html")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True)
-
